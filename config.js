bacbf89d1104bda8e4c64647e21ff59396ab1785
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-06-26 20:53:39 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+/** swap out */
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/d5b2a8ac-b8d4-49a3-8b7b-62509f6dd4c9/token";
+
+/** swap out */
+const instanceLocator = "v1:us1:d5b2a8ac-b8d4-49a3-8b7b-62509f6dd4c9";
+
+export { tokenUrl, instanceLocator }
